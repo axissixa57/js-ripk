62f279bc75e42a904b6bff348ee5d06bbecc956a
Yauheni <zhenyaaxis@gmail.com>
2019-04-02 12:11:44 +0300

    feat: add task_5

---
+++ task_5/task_5.js
@@ -0,0 +1,7 @@
+let header = document.createElement("header");
+document.body.appendChild(header);
+let main = document.createElement("main");
+document.body.appendChild(main);
+let aside = document.createElement("aside");
+let section = document.createElement("section");
+//console.log(header.children);
