42b7cca81c3e32adfce9d3f714f14bfaad9dbfce
Ax1S <axissixa@mail.ru>
2019-04-11 23:21:56 +0300

    update task 6.5

--- task_6/5/task5.js
+++ task_6/5/task5.js
@@ -3,6 +3,9 @@ const input = document.querySelectorAll(".answer");
 const button = document.getElementById('btn');
 const points = document.getElementById("points");
 const numOfWords = document.getElementById("numOfWords");
+let td = document.getElementsByTagName('td');
+
+input[0].value.toUpperCase();
 points.innerHTML = 0;
 numOfWords.innerHTML = 0;
 
@@ -27,25 +30,34 @@ function isFullOpen() {
     else return false;
 }
 
-let randWord = generateWord(); 
+let randWord = generateWord();
 let mistakes = 0;
 console.log(randWord);
 
-button.addEventListener("click", function() {
-    let inputValue = input[0].value.toUpperCase();
 
-    for (let i = 0; i < randWord.length; i++) {
-        if (randWord[i] == inputValue) {
-            divMainArea[0].children[i].classList.add("revealed");
+for (let i = 0; i < td.length; i++) {
+    td[i].addEventListener('click', () => {
+        if (randWord.search(td[i].innerHTML) != -1) {
+            td[i].classList.add("green");
+            for (let k = 0; k < randWord.length; k++) {
+                if (randWord[k] == td[i].innerHTML) {
+                    divMainArea[0].children[k].classList.add("revealed");
+                }
+            }
+        } else {
+            td[i].classList.add("red");
+            mistakes++;
         }
+    });
+}
+
+button.addEventListener("click", function () {
+    if(input[0].value == randWord) {
+        alert(`Вы угадали! Количество неправильных букв = ${mistakes}`);
+        mistakes = 0;
     }
 
-    if(randWord.search(inputValue) == -1) {
-        mistakes++;
-        alert(`Такой буквы здесь нет! Ошибок: ${mistakes}`);
-    }
-    
-    if(isFullOpen()) {
+    if (isFullOpen()) {
         points.innerHTML = (parseInt(points.innerHTML) + 70).toString();
         numOfWords.innerHTML = (parseInt(numOfWords.innerHTML) + 1).toString();
 
@@ -53,19 +65,76 @@ button.addEventListener("click", function() {
             for (let i = 0; i < divMainArea[0].children.length; i++) {
                 divMainArea[0].children[i].classList.remove("revealed");
             }
+
+            for (let j = 0; j < td.length; j++) {
+                if (td[j].classList.contains("red")) {
+                    td[j].classList.remove("red");
+                } else if (td[j].classList.contains("green")) {
+                    td[j].classList.remove("green");
+                }
+            }
+
             randWord = generateWord();
         }, 3000);
-
-        alert(`Вы угадали! Количество неправильных букв = ${mistakes}`);
-        mistakes = 0;
     }
 
-    if(input[0].value.length == 0) {
-        alert('Введите букву!');
-    }
-
-    input[0].value = "";
+    //input[0].value = "";
 });
+
+
+// for (let i = 0; i < td.length; i++) {
+//     td[i].addEventListener('click', () => {
+
+//         for (let j = 0; j < divMainArea[0].children.length; j++) {
+//             if (td[i].innerHTML == divMainArea[0].children[j].innerHTML) {
+//                 td[i].classList.add("green");
+//                 for (let k = 0; k < randWord.length; k++) {
+//                     if (randWord[k] == td[i].innerHTML) {
+//                         divMainArea[0].children[k].classList.add("revealed");
+//                     }
+//                 }
+//             } else {
+//                 td[i].classList.add("red");
+//             }
+//         }
+//     });
+// }
+
+// if (isFullOpen()) {
+//     points.innerHTML = (parseInt(points.innerHTML) + 70).toString();
+//     numOfWords.innerHTML = (parseInt(numOfWords.innerHTML) + 1).toString();
+
+//     setTimeout(function () {
+//         for (let i = 0; i < divMainArea[0].children.length; i++) {
+//             divMainArea[0].children[i].classList.remove("revealed");
+//         }
+//         for (let j = 0; j < td.length; j++) {
+//             if (td[j].classList.contains("green")) {
+//                 td[j].classList.remove("green");
+//             } else if (td[j].classList.contains("red")) {
+//                 td[j].classList.remove("red");
+//             }
+//         }
+
+//         randWord = generateWord();
+//     }, 3000);
+
+//     alert(`Вы угадали! Количество неправильных букв = ${mistakes}`);
+//     mistakes = 0;
+// }
+
+// if (randWord.search(inputValue) == -1) {
+//     mistakes++;
+//     alert(`Такой буквы здесь нет! Ошибок: ${mistakes}`);
+// }
+
+
+
+
+
+
+
+
 
 
 
