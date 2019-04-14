cd6bd289429bddd1b328ae08a924d915e13b17a7
Ax1S <axissixa@mail.ru>
2019-04-14 21:54:27 +0300

    feat: complete task 7.3 without the additional task

--- task_7/3/loader.js
+++ task_7/3/loader.js
@@ -4,6 +4,7 @@ const valueBox = document.querySelector(".value-box");
 const sectionFrame1 = document.getElementById('frame_1');
 const sectionFrame2 = document.getElementById('frame_2');
 const sectionFrame3 = document.getElementById('frame_3');
+const sectionFrame4 = document.getElementById('frame_4');
 const buttonRegistry = document.querySelector('#frame_1 > button');
 const buttonNext = document.getElementById('next');
 const buttonCancel = document.getElementById('cancel');
@@ -18,86 +19,125 @@ const name = document.getElementById('name');
 const surname = document.getElementById('surname');
 let progress = 0;
 
-let timerId = setInterval(function () {
-    if (progress > 100) {
-        clearInterval(timerId);
-        loader.style.display = "none";
-        page.style.display = "flex";
-    }
-    progress += 5;
-    valueBox.style.width = `${progress}%`;
-}, 75);
+// let timerId = setInterval(function () {
+//     if (progress > 100) {
+//         clearInterval(timerId);
+//         loader.style.display = "none";
+//         page.style.display = "flex";
+//     }
+//     progress += 5;
+//     valueBox.style.width = `${progress}%`;
+// }, 75);
 
-buttonRegistry.addEventListener('click', () => {
-    sectionFrame1.style.display = "none";
-    sectionFrame2.style.display = "flex";
-});
+// buttonRegistry.addEventListener('click', () => {
+//     sectionFrame1.style.display = "none";
+//     sectionFrame2.style.display = "flex";
+// });
 
-buttonCancel.addEventListener('click', () => {
-    sectionFrame2.style.display = "none";
-    sectionFrame1.style.display = "flex";
-});
+// buttonCancel.addEventListener('click', () => {
+//     sectionFrame2.style.display = "none";
+//     sectionFrame1.style.display = "flex";
+// });
 
-buttonNext.addEventListener('click', () => {
-    if (validate()) {
-        sectionFrame2.style.display = "none";
-        sectionFrame3.style.display = "flex";
-    }
-});
+// buttonNext.addEventListener('click', () => {
+//     if (validate()) {
+//         sectionFrame2.style.display = "none";
+//         sectionFrame3.style.display = "flex";
+//     }
+// });
 
-function inputsHasValues() {
-    let check = true;
+// function inputsHasValues() {
+//     let check = true;
 
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].value == "") {
-            check = false;
-            break;
-        }
-    }
-    return check;
-}
+//     for (let i = 0; i < inputs.length; i++) {
+//         if (inputs[i].value == "") {
+//             check = false;
+//             break;
+//         }
+//     }
+//     return check;
+// }
 
-for (let i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('input', () => {
-        if (inputsHasValues()) {
-            if (pass.value == repeatPass.value) {
-                buttonNext.removeAttribute('disabled');
-                repeatPass.style.borderBottom = '1.5px solid #a397ff';
-            } else if (pass.value != repeatPass.value) {
-                repeatPass.style.borderBottom = '1.5px solid #ff0000';
-                buttonNext.setAttribute('disabled', 'disabled');
-            } else {
-                buttonNext.setAttribute('disabled', 'disabled');
-            }
-        }
-    });
-}
+// for (let i = 0; i < inputs.length; i++) {
+//     inputs[i].addEventListener('input', () => {
+//         if (inputsHasValues()) {
+//             if (pass.value == repeatPass.value) {
+//                 buttonNext.removeAttribute('disabled');
+//                 repeatPass.style.borderBottom = '1.5px solid #a397ff';
+//             } else if (pass.value != repeatPass.value) {
+//                 repeatPass.style.borderBottom = '1.5px solid #ff0000';
+//                 buttonNext.setAttribute('disabled', 'disabled');
+//             } else {
+//                 buttonNext.setAttribute('disabled', 'disabled');
+//             }
+//         }
+//     });
+// }
 
-for (let i = 0; i < countries.length; i++) {
-    let option = document.createElement('option');
-    option.innerHTML = `${countries[i]}`;
-    select.appendChild(option);
-}
+// for (let i = 0; i < countries.length; i++) {
+//     let option = document.createElement('option');
+//     option.innerHTML = `${countries[i]}`;
+//     select.appendChild(option);
+// }
 
-function validate() {
-    let regEmail = /^[a-zA-Z0-9!#$%&'*+\-=?^_`{|}~]+@[a-z]+\.[a-z]{2,4}$/;
-    let regNameAndSurname = /^[a-zA-Zа-яА-ЯёЁ]+$/;
-    if (regNameAndSurname.test(name.value) == false) {
-        alert('Введите корректное имя');
+// function validate() {
+//     let regEmail = /^[a-zA-Z0-9!#$%&'*+\-=?^_`{|}~]+@[a-z]+\.[a-z]{2,4}$/;
+//     let regNameAndSurname = /^[a-zA-Zа-яА-ЯёЁ]+$/;
+//     if (regNameAndSurname.test(name.value) == false) {
+//         alert('Введите корректное имя');
+//         return false;
+//     }
+
+//     if (regEmail.test(email.value) == false) {
+//         alert('Введите корректный e-mail');
+//         return false;
+//     }
+
+//     if (regNameAndSurname.test(surname.value) == false) {
+//         alert('Введите корректную фамилию');
+//         return false;
+//     }
+
+//     return true;
+// }
+
+const buttonSend = document.getElementById('send');
+const buttonCheck = document.getElementById('check');
+const buttonNextOfPhonePage = document.getElementById('nextPhone');
+const inputPhone = document.getElementById('phone');
+const inputCode = document.getElementById('code');
+const blockConfirmationCode = document.querySelector('.confirmation-code');
+
+
+function validatePhoneNumber() {
+    let regPhoneNumber = /^[+]{1}[0-9]{3}[(]{1}[0-9]{2}[)]{1}[0-9]{3}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$/;
+    if (regPhoneNumber.test(inputPhone.value) == false) {
+        alert('Введите корректный номер телефона');
+        inputPhone.value = "";
         return false;
     }
-
-    if (regEmail.test(email.value) == false) {
-        alert('Введите корректный e-mail');
-        return false;
-    }
-
-    if (regNameAndSurname.test(surname.value) == false) {
-        alert('Введите корректную фамилию');
-        return false;
-    }
-
     return true;
 }
 
+buttonSend.addEventListener('click', () => {
+    if (validatePhoneNumber()) {
+        blockConfirmationCode.style.visibility = 'visible';
+    } else {
+        blockConfirmationCode.style.visibility = 'hidden';
+    }
+});
 
+buttonCheck.addEventListener('click', () => {
+    if(inputCode.value.length == 4) {
+        console.log('Yes');
+        buttonNextOfPhonePage.removeAttribute('disabled');
+    } else {
+        console.log('No');
+        buttonNext.setAttribute('disabled', 'disabled');
+    }
+});
+
+buttonNextOfPhonePage.addEventListener('click', () => {
+    sectionFrame3.style.display = "none";
+    sectionFrame4.style.display = "flex";
+});
