0253e86641017397ba6db2115bad11fe40ea9ba4
Ax1S <axissixa@mail.ru>
2019-04-14 12:34:05 +0300

    update task 7.2

--- task_7/2/loader.js
+++ task_7/2/loader.js
@@ -6,31 +6,41 @@ const sectionFrame2 = document.getElementById('frame_2');
 const sectionFrame3 = document.getElementById('frame_3');
 const buttonRegistry = document.querySelector('#frame_1 > button');
 const buttonNext = document.getElementById('next');
+const buttonCancel = document.getElementById('cancel');
 const form = document.querySelector('form');
 const inputs = document.getElementsByTagName('input');
 const pass = document.getElementById('pass');
 const repeatPass = document.getElementById('repeat_pass');
 let progress = 0;
+const countries = ["Абхазия", "Австралия", "Австрия", "Азербайджан", "Албания", "Алжир", "Ангола", "Ангуилья", "Андорра", "Антигуа и Барбуда", "Антильские острова", "Аргентина", "Армения", "Афганистан", "Багамские острова", "Бангладеш", "Барбадос", "Бахрейн", "Беларусь", "Белиз", "Бельгия", "Бенин", "Бермуды", "Болгария", "Боливия", "Босния/Герцеговина", "Ботсвана", "Бразилия", "Британские Виргинские о-ва", "Бруней", "Буркина Фасо", "Бурунди", "Бутан", "Вануату", "Ватикан", "Великобритания", "Венгрия", "Венесуэла", "Вьетнам", "Габон", "Гаити", "Гайана", "Гамбия", "Гана", "Гваделупа", "Гватемала", "Гвинея", "Гвинея-Бисау", "Германия", "Гернси остров", "Гибралтар", "Гондурас", "Гонконг", "Государство Палестина", "Гренада", "Гренландия", "Греция", "Грузия", "ДР Конго", "Дания", "Джерси остров", "Джибути", "Доминиканская Республика", "Египет", "Замбия", "Западная Сахара", "Зимбабве", "Израиль", "Индия", "Индонезия", "Иордания", "Ирак", "Иран", "Ирландия", "Исландия", "Испания", "Италия", "Йемен", "Кабо-Верде", "Казахстан", "Камбоджа", "Камерун", "Канада", "Катар", "Кения", "Кипр", "Китай", "Колумбия", "Коста-Рика", "Кот-д'Ивуар", "Куба", "Кувейт", "Кука острова", "Кыргызстан", "Лаос", "Латвия", "Лесото", "Либерия", "Ливан", "Ливия", "Литва", "Лихтенштейн", "Люксембург", "Маврикий", "Мавритания", "Мадагаскар", "Македония", "Малайзия", "Мали", "Мальдивские острова", "Мальта", "Марокко", "Мексика", "Мозамбик", "Молдова", "Монако", "Монголия", "Мьянма (Бирма)", "Мэн о-в", "Намибия", "Непал", "Нигер", "Нигерия", "Нидерланды (Голландия)", "Никарагуа", "Новая Зеландия", "Новая Каледония", "Норвегия", "О.А.Э.", "Оман", "Пакистан", "Палау", "Панама", "Папуа Новая Гвинея", "Парагвай", "Перу", "Питкэрн остров", "Польша", "Португалия", "Пуэрто Рико", "Республика Конго", "Реюньон", "Россия","Руанда", "Румыния", "США", "Сальвадор", "Самоа", "Сан-Марино", "Сан-Томе и Принсипи", "Саудовская Аравия", "Свазиленд", "Святая Люсия", "Северная Корея", "Сейшеллы", "Сен-Пьер и Микелон", "Сенегал", "Сент Китс и Невис", "Сент-Винсент и Гренадины", "Сербия", "Сингапур", "Сирия", "Словакия", "Словения", "Соломоновы острова", "Сомали", "Судан", "Суринам", "Сьерра-Леоне", "Таджикистан", "Таиланд", "Тайвань", "Танзания", "Того", "Токелау острова", "Тонга", "Тринидад и Тобаго", "Тувалу", "Тунис", "Туркменистан", "Туркс и Кейкос", "Турция", "Уганда", "Узбекистан", "Украина", "Уоллис и Футуна острова", "Уругвай", "Фарерские острова", "Фиджи", "Филиппины", "Финляндия", "Франция", "Французская Полинезия", "Хорватия", "Чад", "Черногория", "Чехия", "Чили", "Швейцария", "Швеция", "Шри-Ланка", "Эквадор", "Экваториальная Гвинея", "Эритрея", "Эстония", "Эфиопия", "ЮАР", "Южная Корея", "Южная Осетия", "Ямайка", "Япония",];
+const select = document.getElementById('countries');
 
-let timerId = setInterval(function () {
-    if (progress > 100) {
-        clearInterval(timerId);
-        loader.style.display = "none";
-        page.style.display = "flex";
-    }
-    progress += 5;
-    valueBox.style.width = `${progress}%`;
-}, 75);
+//console.log(countries);
 
-buttonRegistry.addEventListener('click', () => {
-    sectionFrame1.style.display = "none";
-    sectionFrame2.style.display = "flex";
-});
+// let timerId = setInterval(function () {
+//     if (progress > 100) {
+//         clearInterval(timerId);
+//         loader.style.display = "none";
+//         page.style.display = "flex";
+//     }
+//     progress += 5;
+//     valueBox.style.width = `${progress}%`;
+// }, 75);
 
-buttonNext.addEventListener('click', () => {
-    sectionFrame2.style.display = "none";
-    sectionFrame3.style.display = "flex";
-});
+// buttonRegistry.addEventListener('click', () => {
+//     sectionFrame1.style.display = "none";
+//     sectionFrame2.style.display = "flex";
+// });
+
+// buttonCancel.addEventListener('click', () => {
+//     sectionFrame2.style.display = "none";
+//     sectionFrame1.style.display = "flex";
+// });
+
+// buttonNext.addEventListener('click', () => {
+//     sectionFrame2.style.display = "none";
+//     sectionFrame3.style.display = "flex";
+// });
 
 function inputsHasValues() {
     let check = true;
@@ -49,7 +59,16 @@ for (let i = 0; i < inputs.length; i++) {
         if(inputsHasValues()) {
             if(pass.value == repeatPass.value) {
                 buttonNext.removeAttribute('disabled');
+            } else {
+                buttonNext.setAttribute('disabled', 'disabled');
             }
         } 
     });
 }
+
+for(let i = 0; i < countries.length; i++) {
+    let option = document.createElement('option');
+    option.innerHTML = `${countries[i]}`;
+    select.appendChild(option);
+}
+
