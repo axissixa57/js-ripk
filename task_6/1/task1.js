b473028fbefbf344ac18997bc1635efa4516e9a9
Ax1S <axissixa@mail.ru>
2019-07-02 19:43:30 +0300

    test github.io

--- task_6/1/task1.js
+++
@@ -1,7 +0,0 @@
-const divMainArea = document.getElementsByClassName("main-area");
-
-for (let i = 0; i < divMainArea[0].children.length; i++) {
-    divMainArea[0].children[i].addEventListener("click", () => {
-        divMainArea[0].children[i].classList.toggle("revealed");
-    })
-}
